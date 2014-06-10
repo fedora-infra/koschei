e8e4afdddd099dd6ab18274af39e660ff561a6a5
Michael Simacek <msimacek@redhat.com>
2014-06-10 15:47:22 +0200

    Migrate dependency plugin to new plugin API

--- plugins/dependency.py
+++ plugins/dependency.py
@@ -1,37 +1,78 @@
-from models import PriorityChange, Dependency, current_priorities
-from plugins import plugin
+from sqlalchemy import Column, Integer, ForeignKey, Boolean, String
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql.expression import func
 
-@plugin('repo_done')
-def apply_depchange_priority(db_session):
-    current_priorities(db_session).filter_by(plugin_name='dependency')\
-                                  .update({'effective': True})
-    db_session.commit()
+from models import Dependency, BuildTrigger, Base, Package
+from plugins import Plugin
 
-def add_dependency_change(db_session, dependency, package, new_priority):
-    change = PriorityChange(package_id=package.id, effective=False,
-                            plugin_name='dependency', value=new_priority,
-                            comment='Dependency {} updated'\
-                                    .format(dependency.name))
-    db_session.add(change)
-    db_session.commit()
+class DependencyUpdate(Base):
+    __tablename__ = 'dependency_update'
 
-@plugin('build_tagged')
-def package_updated(db_session, package):
-    visited = set()
-    def recursive_update(pkgs, level=1):
-        new_priority = 30 // level # TODO Bulgarian constant
-        if new_priority:
-            pkg_ids = [pkg.id for pkg in pkgs]
-            visited.update(pkg_ids)
-            deps = db_session.query(Dependency)\
-                   .filter(Dependency.dependency_id.in_(pkg_ids),
-                           Dependency.runtime == (level != 1))
-            pkgs_on_level = [dep.package for dep in deps if dep.package_id
-                             not in visited]
-            if pkgs_on_level:
-                for pkg in pkgs_on_level:
-                    if pkg.watched:
-                        add_dependency_change(db_session, package, pkg, new_priority)
-                recursive_update(pkgs_on_level, level + 1)
+    id = Column(Integer, primary_key=True)
+    package_id = Column(ForeignKey('package.id'), nullable=False)
+    dependency_id = Column(ForeignKey('package.id'), nullable=False)
+    package = relationship('Package', primaryjoin=(package_id == Package.id))
+    dependency = relationship('Package', primaryjoin=(dependency_id == Package.id))
+    effective = Column(Boolean, nullable=False, default=False)
+    weight = Column(Integer, nullable=False)
+    version = Column(String)
+    release = Column(String)
 
-    recursive_update({package})
+class DependencyPlugin(Plugin):
+    def __init__(self):
+        super(DependencyPlugin, self).__init__()
+        self.register_event('repo_done', self.apply_updates)
+        self.register_event('build_tagged', self.package_updated)
+        self.register_event('get_priority_query', self.get_priority_query)
+        self.register_event('build_submitted', self.populate_triggers)
+
+    def get_priority_query(self, db_session):
+        q = db_session.query(DependencyUpdate.package_id,
+                             func.sum(DependencyUpdate.weight))\
+                      .filter(DependencyUpdate.effective == True)\
+                      .group_by(DependencyUpdate.package_id)
+        return q.subquery()
+
+    def populate_triggers(self, db_session, build):
+        updates = db_session.query(DependencyUpdate)\
+                            .filter_by(effective=True,
+                                       package_id=build.package.id)
+        for update in updates:
+            comment = 'Dependency {} updated to version {}-{}'\
+                      .format(update.dependency.name, update.version,
+                              update.release)
+            trigger = BuildTrigger(build_id=build.id, comment=comment)
+            db_session.add(trigger)
+            db_session.commit()
+        updates.delete()
+        db_session.commit()
+
+    def apply_updates(self, db_session):
+        db_session.query(DependencyUpdate).update({'effective': True})
+        db_session.commit()
+
+    def package_updated(self, db_session, package, version, release):
+        visited = set()
+        def recursive_update(pkgs, level=1):
+            new_priority = 30 // level # TODO Bulgarian constant
+            if new_priority:
+                pkg_ids = [pkg.id for pkg in pkgs]
+                visited.update(pkg_ids)
+                deps = db_session.query(Dependency)\
+                       .filter(Dependency.dependency_id.in_(pkg_ids),
+                               Dependency.runtime == (level != 1))
+                pkgs_on_level = [dep.package for dep in deps if dep.package_id
+                                 not in visited]
+                if pkgs_on_level:
+                    for pkg in pkgs_on_level:
+                        if pkg.watched:
+                            update = DependencyUpdate(package_id=pkg.id,
+                                                      weight=new_priority,
+                                                      dependency_id=package.id,
+                                                      version=version,
+                                                      release=release)
+                            db_session.add(update)
+                            db_session.commit()
+                    recursive_update(pkgs_on_level, level + 1)
+
+        recursive_update({package})
