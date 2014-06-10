bacaecc5cd9f5dcd1333dce4fbbb390ea55012e1
Michael Simacek <msimacek@redhat.com>
2014-06-10 15:46:33 +0200

    Rework plugin API

--- plugins.py
+++ plugins.py
@@ -1,33 +1,50 @@
 import imp
 import os
 
-from functools import wraps
 from collections import defaultdict
 
 plugin_dir = os.path.join(os.path.dirname(__file__), 'plugins')
 
-plugins = []
-
-plugin_hooks = defaultdict(list)
+plugins = {}
+ordered_plugins = []
+plugin_classes = []
 
 def load_plugins():
+    if plugins:
+        return
     for path in os.listdir(plugin_dir):
         if path.endswith('.py'):
             name = path[:-3]
             descriptor = imp.find_module(name, [plugin_dir])
+            print 'Loading {}'.format(name)
             imp.load_module(name, *descriptor)
+    for cls in plugin_classes:
+        plugin = cls()
+        plugins[name] = plugin
+        key = '{}-{}'.format(plugin.order, name)
+        ordered_plugins.append((key, plugin))
+    ordered_plugins.sort()
 
-def plugin(*hooks):
-    def decorator(fn):
-        @wraps(fn)
-        def decorated(*args, **kwargs):
-            fn(*args, **kwargs)
-        plugins.append(decorated)
-        for hook in hooks:
-            plugin_hooks[hook].append(decorated)
-        return decorated
-    return decorator
+def dispatch_event(event_name, *args, **kwargs):
+    ret = []
+    for _, plugin in ordered_plugins:
+        for method in plugin._event_hooks[event_name]:
+            ret.append(method(*args, **kwargs))
+    return ret
 
-def call_hooks(hook, *args, **kwargs):
-    for fn in plugin_hooks[hook]:
-        fn(*args, **kwargs)
+class _Meta(type):
+    def __init__(cls, name, bases, members):
+        super(_Meta, cls).__init__(name, bases, members)
+        if name != 'Plugin':
+            plugin_classes.append(cls)
+
+class Plugin(object):
+    __metaclass__ = _Meta
+
+    order = 50
+
+    def __init__(self):
+        self._event_hooks = defaultdict(list)
+
+    def register_event(self, event_name, method):
+        self._event_hooks[event_name].append(method)
