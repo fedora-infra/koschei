30ecc899f8b12045af5a424247dff706236ff7e9
Michael Simacek <msimacek@redhat.com>
2014-06-22 16:10:25 +0200

    Remove dependency adding from admin

--- admin.py
+++ admin.py
@@ -2,7 +2,7 @@
 import sys
 import md5
 
-from koschei import plugins, dep_loader
+from koschei import plugin
 from koschei.models import *
 
 if __name__ == '__main__':
@@ -27,18 +27,6 @@ if __name__ == '__main__':
             pkg.static_priority = prio
         s.commit()
         plugins.dispatch_event('add_package', s, pkg)
-    elif cmd == 'adddep':
-        pkgname = sys.argv[2]
-        depname = sys.argv[3]
-        pkg = s.query(Package).filter_by(name=pkgname).one()
-        dep = s.query(Package).filter_by(name=depname).first()
-        if not dep:
-            dep = Package(name=depname, watched=False)
-            s.add(dep)
-            s.commit()
-        assoc = Dependency(package_id=pkg.id, dependency_id=dep.id)
-        s.add(assoc)
-        s.commit()
     else:
         print('No such command')
         sys.exit(1)
