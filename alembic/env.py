9ced1fc4062ae22049724d6c3eb0e2d8af0cb125
Michael Simacek <msimacek@redhat.com>
2014-06-26 17:36:06 +0200

    Enable autogenerating migrations

--- alembic/env.py
+++ alembic/env.py
@@ -1,6 +1,10 @@
 from __future__ import with_statement
 from alembic import context
 from logging.config import fileConfig
+from koschei.models import Base
+
+# loads tables from plugins
+from koschei import plugin
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -14,7 +18,7 @@ fileConfig(config.config_file_name)
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
