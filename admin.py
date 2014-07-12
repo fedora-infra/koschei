d00b87280b2b43dd375501df5e10a78ea653444c
Michael Simacek <msimacek@redhat.com>
2014-07-12 16:19:00 +0200

    Rewrite admin script

--- admin.py
+++ admin.py
@@ -1,49 +1,97 @@
 #!/usr/bin/python
-import sys
+from __future__ import print_function
 
-from koschei.models import *
+import sys
+import argparse
+
+from koschei.models import engine, Session, Base, Package
 from koschei import util
 
-if __name__ == '__main__':
-    cmd = sys.argv[1]
-    s = Session()
-    if cmd == 'createdb':
-        Base.metadata.create_all(engine)
+def fail(msg):
+    print(msg, file=sys.stderr)
+    sys.exit(1)
+
+class Command(object):
+    needs_db = True
+    needs_koji = False
+
+    def setup_parser(self, parser):
+        pass
+
+    def execute(self, **kwargs):
+        raise NotImplementedError()
+
+def main():
+    main_parser = argparse.ArgumentParser()
+    subparser = main_parser.add_subparsers()
+    for Cmd in Command.__subclasses__():
+        cmd_name = Cmd.__name__.lower()
+        cmd = Cmd()
+        parser = subparser.add_parser(cmd_name, help=cmd.__doc__)
+        cmd.setup_parser(parser)
+        parser.set_defaults(cmd=cmd)
+    args = main_parser.parse_args()
+    cmd = args.cmd
+    kwargs = vars(args)
+    del kwargs['cmd']
+    db_session = None
+    if cmd.needs_db:
+        db_session = Session()
+        kwargs['db_session'] = db_session
+    if cmd.needs_koji:
+        kwargs['koji_session'] = util.create_koji_session(anonymous=True)
+    cmd.execute(**kwargs)
+    if db_session:
+        db_session.commit()
+        db_session.close()
+
+class CreateDb(Command):
+    """ Creates database tables """
+
+    needs_db = False
+
+    def execute(self):
         from alembic.config import Config
         from alembic import command
+        Base.metadata.create_all(engine)
         alembic_cfg = Config(util.config['alembic']['alembic_ini'])
         command.stamp(alembic_cfg, "head")
-    elif cmd == 'dropdb':
-        Base.metadata.drop_all(engine)
-    elif cmd == 'addpkg':
-        prio = 0
-        if len(sys.argv) > 3:
-            prio = int(sys.argv[3])
-        name = sys.argv[2]
-        pkg = Package(name=name, static_priority=prio,
-                      manual_priority=30)
-        s.add(pkg)
-        s.commit()
-    elif cmd == 'addpkgs':
-        pkgs = []
-        x = 100
-        for name in sys.argv[2:]:
-            pkg = Package(name=name, manual_priority=x // 3)
-            s.add(pkg)
-            s.commit()
-            pkgs.append(pkg)
-            x -= 1
-    elif cmd == 'setprio':
-        #TODO use argparse
-        if sys.argv[2] == '--static':
-            pkg = s.query(Package).filter_by(name=sys.argv[3]).one()
-            pkg.static_priority = int(sys.argv[4])
-        else:
-            pkg = s.query(Package).filter_by(name=sys.argv[2]).one()
-            pkg.manual_priority = int(sys.argv[3])
-        s.commit()
-    else:
-        print('No such command')
-        sys.exit(1)
 
-    s.close()
+class AddPkg(Command):
+    """ Adds given packages to database """
+
+    def setup_parser(self, parser):
+        parser.add_argument('names', nargs='+')
+        parser.add_argument('-s', '--static-priority', type=int)
+        parser.add_argument('-m', '--manual-priority', type=int)
+        parser.add_argument('-g', '--group', nargs='*')
+
+    #TODO group
+    def execute(self, db_session, names, group, static, manual):
+        for name in names:
+            pkg = db_session.query(Package).filter_by(name=name).first()
+            if not pkg:
+                pkg = Package(name=name)
+                db_session.add(pkg)
+            pkg.static_priority = static or 0
+            pkg.manual_priority = manual or 30
+
+class SetPrio(Command):
+    """ Sets pacakges priority to given value """
+
+    def setup_parser(self, parser):
+        parser.add_argument('name')
+        parser.add_argument('value')
+        parser.add_argument('--static', action='store_true')
+
+    def execute(self, db_session, name, value, static):
+        pkg = db_session.query(Package).filter_by(name=name).first()
+        if not pkg:
+            fail("Package {} not found".format(name))
+        if static:
+            pkg.static_priority = value
+        else:
+            pkg.manual_priority = value
+
+if __name__ == '__main__':
+    main()
