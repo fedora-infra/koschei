7c3bafa9d09f69cbe0bfbf0a5929fd7a5194fa6a
Michael Simacek <msimacek@redhat.com>
2014-07-17 02:04:59 +0200

    Use flask-jinja2 integration

--- koschei/frontend.py
+++ koschei/frontend.py
@@ -1,12 +1,13 @@
 from datetime import datetime
-from flask import Flask, abort
+from flask import Flask, abort, render_template
 from sqlalchemy.orm import scoped_session, sessionmaker, joinedload
 
 from .models import engine, Package
-from .reporter import jinja_env
 from . import util
 
-app = Flask('Koschei')
+dirs = util.config['directories']
+app = Flask('koschei', template_folder=dirs['templates'],
+            static_folder=dirs['static'])
 app.config.from_object(util.config['flask'])
 
 db_session = scoped_session(sessionmaker(autocommit=False, bind=engine))
@@ -19,22 +20,27 @@ if False:
 def shutdown_session(exception=None):
     db_session.remove()
 
+@app.template_filter('date')
+def date_filter(date):
+    return date.strftime("%F %T") if date else ''
+
+@app.context_processor
+def inject_dates():
+    return {'since': datetime.min, 'until': datetime.now()}
+
 @app.route('/')
 def frontpage():
-    since = datetime.min
-    until = datetime.now()
     packages = db_session.query(Package)\
                          .options(joinedload(Package.last_build))\
                          .order_by(Package.id).all()
-    return jinja_env.get_template("frontpage.html")\
-                    .render(packages=packages, since=since, until=until)
+    return render_template("frontpage.html", packages=packages)
 
 @app.route('/package/<name>.html')
 def package_detail(name):
     package = db_session.query(Package).filter_by(name=name).first()
     if not package:
         abort(404)
-    return jinja_env.get_template("package-detail.html").render(package=package)
+    return render_template("package-detail.html", package=package)
 
 
 if __name__ == '__main__':
