71a09ae85b290a648b125646f5b8d5f6ffd226ca
Michael Simacek <msimacek@redhat.com>
2015-01-05 17:21:45 +0100

    [deploy.sh] Build with mock

--- aux/deploy.sh
+++ aux/deploy.sh
@@ -4,15 +4,19 @@ set -e
 cd "$(git rev-parse --show-toplevel)"
 
 MACHINE=koschei.cloud.fedoraproject.org
+MOCK_CONFIG=fedora-20-x86_64
 pushd systemd
 SERVICES="httpd $(echo *.service)"
 popd
 VERSION="$(python setup.py -V)"
-RELEASE="$(rpmspec -q koschei.spec --qf='%{release}')"
 
 git archive HEAD --prefix="koschei-koschei-$VERSION/"| gzip >koschei-${VERSION}.tar.gz
-rpmbuild -bb koschei.spec -D"_sourcedir $PWD" -D"_rpmdir $PWD"
-cat noarch/koschei-$VERSION-${RELEASE}.noarch.rpm | ssh root@$MACHINE "
+DIST=`mock -r $MOCK_CONFIG -q --chroot 'rpm -E "%dist"'`
+RELEASE="$(sed -n 's/^Release:\s*\([^%]*\).*/\1/p' koschei.spec)$DIST"
+mkdir -p result
+mock -r $MOCK_CONFIG -n --buildsrpm --spec koschei.spec --sources . --resultdir result/
+mock -r $MOCK_CONFIG -n --rebuild result/koschei-${VERSION}-${RELEASE}.src.rpm --resultdir result/
+cat result/koschei-$VERSION-${RELEASE}.noarch.rpm | ssh root@$MACHINE "
 
 set -e
 trap 'rm -f koschei.rpm' 0
