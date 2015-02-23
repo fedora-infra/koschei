cc27bacbcb9624c7c5d7c879366a98d410cb3528
Michael Simacek <msimacek@redhat.com>
2015-02-23 16:25:13 +0100

    [aux] Frontend debug launcher

---
+++ aux/debug_frontend.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+
+import logging
+import time
+from sqlalchemy import event
+from sqlalchemy.engine import Engine
+
+from koschei.frontend import app as application
+import koschei.views
+import koschei.auth
+
+logger = logging.getLogger("koschei.sql")
+logger.setLevel(logging.DEBUG)
+
+@event.listens_for(Engine, "before_cursor_execute")
+def before_cursor_execute(conn, cursor, statement, 
+                        parameters, context, executemany):
+    context._query_start_time = time.time()
+    logger.debug("Start Query:\n%s" % statement)
+    logger.debug("Parameters:\n%r" % (parameters,))
+
+
+@event.listens_for(Engine, "after_cursor_execute")
+def after_cursor_execute(conn, cursor, statement, 
+                        parameters, context, executemany):
+    total = time.time() - context._query_start_time
+    logger.debug("Query Complete!")
+
+    logger.debug("Total Time: %.02fms" % (total*1000))
+
+
+application.run(debug=True)
--- koschei.wsgi
+++ koschei.wsgi
@@ -1,6 +1,3 @@
 from koschei.frontend import app as application
 import koschei.views
 import koschei.auth
-
-if __name__ == '__main__':
-    application.run(debug=True)
