33453b370212ab5bfba14a43bbb6fbdbfd264efd
Michael Simacek <msimacek@redhat.com>
2015-09-01 10:26:32 +0200

    Keep jenkins configuration in source tree

---
+++ aux/jenkins.bash
@@ -0,0 +1,9 @@
+virtualenv koscheienv --system-site-packages
+source koscheienv/bin/activate
+
+pip install sqlalchemy==0.9.7
+pip install fedmsg==0.14
+pip install mock==1.0.1
+pip install --upgrade nose
+
+TEST_WITH_FAITOUT=1 koscheienv/bin/nosetests
