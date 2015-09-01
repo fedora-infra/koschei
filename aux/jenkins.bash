47240b06547a6b5b5841330c57e5fbae080506e7
Mikolaj Izdebski <mizdebsk@redhat.com>
2015-09-01 13:36:00 +0200

    Add test coverage to jenkins.bash

--- aux/jenkins.bash
+++ aux/jenkins.bash
@@ -8,7 +8,7 @@ pip install fedmsg==0.14
 pip install mock==1.0.1
 pip install --upgrade nose
 
-TEST_WITH_FAITOUT=1 koscheienv/bin/nosetests --with-xunit
+TEST_WITH_FAITOUT=1 koscheienv/bin/nosetests --with-xunit --with-xcoverage
 
 pylint -f parseable koschei | tee pylint.out
 pep8 koschei/*.py koschei/*/*.py | tee pep8.out
