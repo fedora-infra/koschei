934b3d21c6906f10f3fa5f7c52e755eed13e2821
Mikolaj Izdebski <mizdebsk@redhat.com>
2015-09-01 13:29:06 +0200

    Run pylint and pep8 on Jenkins

--- aux/jenkins.bash
+++ aux/jenkins.bash
@@ -9,3 +9,6 @@ pip install mock==1.0.1
 pip install --upgrade nose
 
 TEST_WITH_FAITOUT=1 koscheienv/bin/nosetests --with-xunit
+
+pylint -f parseable koschei | tee pylint.out
+pep8 koschei/*.py koschei/*/*.py | tee pep8.out
