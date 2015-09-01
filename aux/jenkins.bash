fe7a9426db59360dffef7d0c6841c78d32425c29
Mikolaj Izdebski <mizdebsk@redhat.com>
2015-09-01 13:55:45 +0200

    Install nosexcover and coverage on Jenkins

--- aux/jenkins.bash
+++ aux/jenkins.bash
@@ -7,8 +7,10 @@ pip install sqlalchemy==0.9.7
 pip install fedmsg==0.14
 pip install mock==1.0.1
 pip install --upgrade nose
+pip install --upgrade nosexcover
+pip install --upgrade coverage
 
-TEST_WITH_FAITOUT=1 koscheienv/bin/nosetests --with-xunit --with-xcoverage
+TEST_WITH_FAITOUT=1 koscheienv/bin/nosetests --with-xunit --cover-erase --cover-package=koschei --with-xcoverage
 
 pylint -f parseable koschei | tee pylint.out
 pep8 koschei/*.py koschei/*/*.py | tee pep8.out
