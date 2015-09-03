7f33ef37d144af7045b20a9d1e9572fbde53facb
Michael Simacek <msimacek@redhat.com>
2015-09-03 10:53:22 +0200

    Try to build RPM on jenkins

--- aux/jenkins.bash
+++ aux/jenkins.bash
@@ -14,3 +14,13 @@ TEST_WITH_FAITOUT=1 koscheienv/bin/nosetests --with-xunit --cover-erase --cover-
 
 pylint -f parseable --rcfile aux/pylintrc `find koschei/ admin.py -name '*.py'`
 pep8 koschei/*.py koschei/*/*.py | tee pep8.out
+
+VERSION="$(python setup.py -V)"
+git archive HEAD --prefix="koschei-$VERSION/"| gzip >"koschei-${VERSION}".tar.gz
+mkdir -p build rpms
+cd build
+rm -f *.src.rpm
+sed "s/^Release:[^%]*/&.jenkins$BUILD_NUMBER/" ../koschei.spec > koschei.spec
+rpmbuild -bs -D"_sourcedir $PWD/.." -D"_srcrpmdir $PWD" koschei.spec
+mock -r epel-7-x86_64 --rebuild koschei-*.src.rpm --resultdir ../rpms
+createrepo_c ../rpms
