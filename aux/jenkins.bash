5dd681492f546e8f31daaab9d038c79f7ef3d16e
Michael Simacek <msimacek@redhat.com>
2015-09-08 00:39:45 +0200

    [jenkins] Cleanup old RPMs

--- aux/jenkins.bash
+++ aux/jenkins.bash
@@ -36,4 +36,8 @@ rm -f *.src.rpm
 sed "s/^Release:[^%]*/&.jenkins$BUILD_NUMBER/" ../koschei.spec > koschei.spec
 rpmbuild -bs -D"_sourcedir $PWD/.." -D"_srcrpmdir $PWD" koschei.spec
 mock -r epel-7-x86_64 --rebuild koschei-*.src.rpm --resultdir ../rpms
-createrepo_c ../rpms
+
+cd ../rpms
+# Keep last 5 RPMS (and 5 SRPMS)
+python -c 'import os,glob;map(os.unlink, sorted(glob.glob("*.rpm"), key=os.path.getmtime)[0:-10])'
+createrepo_c .
