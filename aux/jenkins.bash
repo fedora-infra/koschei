1d4cc4e40ee4727d5cd1823571c98f0000684f44
Michael Simacek <msimacek@redhat.com>
2015-10-05 14:44:07 +0200

    [jenkins] Split build script to build RPM separately

--- aux/jenkins.bash
+++ aux/jenkins.bash
@@ -28,17 +28,3 @@ checked_files=`find koschei/ admin.py -name '*.py'`
 pylint --msg-template="{path}:{line}: [{msg_id}({symbol}), {obj}] {msg}" \
     --rcfile aux/pylintrc $checked_files | tee pylint.out
 pep8 --config=aux/pep8.cfg $checked_files | tee pep8.out
-
-VERSION="$(python setup.py -V)"
-git archive HEAD --prefix="koschei-$VERSION/"| gzip >"koschei-${VERSION}".tar.gz
-mkdir -p build rpms
-cd build
-rm -f *.src.rpm
-sed "s/^Release:[^%]*/&.jenkins$BUILD_NUMBER/" ../koschei.spec > koschei.spec
-rpmbuild -bs -D"_sourcedir $PWD/.." -D"_srcrpmdir $PWD" koschei.spec
-mock -r epel-7-x86_64 --rebuild koschei-*.src.rpm --resultdir ../rpms
-
-cd ../rpms
-# Keep last 5 RPMS (and 5 SRPMS)
-python -c 'import os,glob;map(os.unlink, sorted(glob.glob("*.rpm"), key=os.path.getmtime)[0:-10])'
-createrepo_c .
