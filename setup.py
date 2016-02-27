707da87e56a3143b9059e535eb0677d3f398e182
Michael Simacek <msimacek@redhat.com>
2016-02-27 10:17:28 +0100

    [setup.py] Extract version from specfile

--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
+import re
 from setuptools import setup, find_packages
+
+with open('koschei.spec') as specfile:
+    for line in specfile:
+        match = re.match(r'^Version:\s*([0-9.]+)', line)
+        if match:
+            version = match.group(1)
 
 setup(
     name='koschei',
-    version='1.5',
+    version=version,
     description='',
     author='',
     author_email='',
