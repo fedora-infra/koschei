9f7c84f3bb762d866def4a3b058206c1b9dd41fe
Mikolaj Izdebski <mizdebsk@redhat.com>
2016-03-09 12:23:12 +0100

    Fix cache deadlock caused by out-of-order item fetching
    When adding requested cache items, space must be reserved for them in
    advance to ensure there will be capacity available to fetch them.
    
    Since space is reserved when adding requested item, not all prefetched
    keys can be immediately added as requested items, so keys must be
    queued for later addition when space becomes available.
    
    For easier debugging, this commit also improves logging and uses enum
    for internal state tracking.
    
    Fixes #81

--- koschei/cache_manager.py
+++ koschei/cache_manager.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2015 Red Hat, Inc.
+# Copyright (C) 2015-2016 Red Hat, Inc.
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -41,13 +41,17 @@ else:
 class _CacheItem(object):
     """
     Possible cache item states:
-       state      free  wait  work  description
-       REQUESTED   1     1     0    requested for prefetching
-       PREPARING   0     1     1    being prefetched by worker thread
-       PREPARED    0     0     0    prefetched, waiting to be acquired
-       ACQUIRED    0     1     0    used by caller, must be kept
-       RELEASED    1     0     0    released, but kept in MRU cache
-   """
+       state      description
+       PREPARING  being prefetched by worker thread
+       PREPARED   prefetched, waiting to be acquired
+       ACQUIRED   used by caller, must be kept
+       RELEASED   released, but kept in MRU cache
+    """
+    REQUESTED = "REQUESTED"
+    PREPARING = "PREPARING"
+    PREPARED = "PREPARED"
+    ACQUIRED = "ACQUIRED"
+    RELEASED = "RELEASED"
 
     def __init__(self, key, bank):
         self._key = key
@@ -56,10 +60,13 @@ class _CacheItem(object):
         self._index = 0
         self._next = None
         self._bank = bank
-        # initial state: REQUESTED
-        self._free = True
-        self._wait = True
-        self._work = False
+        self._state = None
+
+    def _transition(self, prev_state, next_state):
+        _log.debug("Item {key} (bank {bank_id}): transition {prev} -> {next}".format(
+            bank_id=self._bank._id, key=self._key, prev=prev_state, next=next_state))
+        assert self._state == prev_state
+        self._state = next_state
 
     def _prepare(self):
         self._value = self._bank._factory.create(self._key, self._next._value
@@ -72,7 +79,7 @@ class _CacheItem(object):
 
 
 class _CacheBank(object):
-    def __init__(self, factory, capacity, max_threads):
+    def __init__(self, id, factory, capacity, max_threads):
         """
         Create cache bank.  Params:
           factory     - object supplied by caller, used to create and
@@ -81,6 +88,7 @@ class _CacheBank(object):
           max_threads - max number of threads that can be working on
                         producing items for this bank
         """
+        self._id = id
         self._factory = factory
         self._capacity = capacity
         self._max_threads = max_threads
@@ -97,43 +105,42 @@ class _CacheBank(object):
             item._index = item._index + 1
         mru._index = 0
 
-    def _count_requested(self):
-        """ Count requested items (state: REQUESTED) """
-        return sum(1 for item in self._items if item._free and item._wait)
-
     def _count_hard(self):
         """ Count hard items (states: PREPARING, PREPARED, ACQUIRED) """
-        return sum(1 for item in self._items if not item._free)
+        return sum(1 for item in self._items if item._state != _CacheItem.RELEASED)
 
     def _count_work(self):
         """ Count items being worked on (state: PREPARING) """
-        return sum(1 for item in self._items if item._work)
+        return sum(1 for item in self._items if item._state == _CacheItem.PREPARING)
 
     def _count_soft(self):
-        """ Count soft items (states: PREPARING, PREPARED, ACQUIRED, RELEASED) """
-        return len(self._items) - self._count_requested()
+        """ Count soft items (states: REQUESTED, PREPARING, PREPARED, ACQUIRED, RELEASED) """
+        return len(self._items)
 
-    def _lru(self, wait):
-        """ Find least-recently used item (either REQUESTED or RELEASED) """
+    def _lru(self, state):
+        """ Find least-recently used item with given state """
         max_idx = 0
         lru = None
         for item in self._items:
-            if item._free and item._wait == wait and item._index >= max_idx:
+            if item._state == state and item._index >= max_idx:
                 max_idx = item._index
                 lru = item
         return lru
 
     def _discard_lru(self):
         """ Remove least-recently used item with state RELEASED """
-        lru = self._lru(False)
+        lru = self._lru(_CacheItem.RELEASED)
         assert lru
         lru._release()
+        lru._transition(_CacheItem.RELEASED, None)
         self._items = [item for item in self._items if item._key != lru._key]
 
-    def _add(self, key):
-        """ Add new REQUESTED item """
+    def _add(self, key, state):
+        """ Add new item with specified state """
         item = _CacheItem(key, self)
+        item._transition(None, state)
         self._items.append(item)
+        self._access(item)
         return item
 
 
@@ -150,7 +157,7 @@ class CacheManager(object):
         self._banks = []
         self._threads = []
         self._terminate = False
-        self._l1 = None
+        self._prefetch_q = []
 
         while len(self._threads) < max_threads:
             thread = Thread(target=self._thread_proc)
@@ -159,37 +166,66 @@ class CacheManager(object):
             self._threads.append(thread)
 
     def add_bank(self, item_factory, capacity, max_threads):
-        bank = _CacheBank(item_factory, capacity, max_threads)
-        if not self._banks:
-            self._l1 = bank
+        id = len(self._banks) + 1
+        bank = _CacheBank(id, item_factory, capacity, max_threads)
         self._banks.append(bank)
 
         initial_cache = item_factory.populate_cache()
         if initial_cache:
             for key, value in initial_cache:
-                item = bank._add(key)
-                item._wait = False
+                item = bank._add(key, _CacheItem.RELEASED)
                 item._value = value
-                bank._access(item)
             while bank._count_soft() > capacity:
                 bank._discard_lru()
 
+    def _add_requested_items(self):
+        if not self._prefetch_q:
+            _log.debug("_ari: Empty prefetch queue")
+        for key in self._prefetch_q:
+            _log.debug("_ari: Processing item {key}".format(key=key))
+            item = self._banks[0]._lookup(key)
+            if item and item._state != _CacheItem.RELEASED:
+                _log.debug("_ari: Item {key} already used, can't add it again".format(key=key))
+                continue
+            for bank in self._banks:
+                item = bank._lookup(key)
+                if item:
+                    assert item._state == _CacheItem.RELEASED
+                    break
+                elif bank._count_work() >= bank._max_threads or bank._count_hard() >= bank._capacity:
+                    _log.debug("_ari: Item {key} can't be added because bank {bank_id} has reached"
+                               " capacity or thread limits".format(key=key, bank_id=bank._id))
+                    return
+            prev_item = None
+            for bank in self._banks:
+                item = bank._lookup(key)
+                if item:
+                    if prev_item:
+                        prev_item._next = item
+                    item._transition(_CacheItem.RELEASED, _CacheItem.PREPARED)
+                    item._next = None
+                    if bank._id == 1:
+                        self._sack_avail.notify()
+                    else:
+                        self._work_avail.notify()
+                    break
+                while bank._count_soft() >= bank._capacity:
+                    bank._discard_lru()
+                _log.debug("_ari: Adding item {key} into bank {bank_id}"
+                           .format(key=key, bank_id=bank._id))
+                item = bank._add(key, _CacheItem.REQUESTED)
+                if prev_item:
+                    prev_item._next = item
+                prev_item = item
+            self._prefetch_q.remove(key)
+
     def _get_item_to_process(self):
         for bank in self._banks:
-            if not bank._count_requested():
-                continue
-            item = bank._lru(True)
-            if not item:
-                continue
-            if item._next and (item._next._free or item._next._wait):
-                continue
-            if bank._count_work() >= bank._max_threads:
-                continue
-            if bank._count_hard() >= bank._capacity:
-                continue
-            while bank._count_soft() >= bank._capacity:
-                bank._discard_lru()
-            return item
+            for item in bank._items:
+                if item._state == _CacheItem.REQUESTED:
+                    if item._next and (item._next._state != _CacheItem.PREPARED):
+                        continue
+                    return item
         return None
 
     def _thread_proc(self):
@@ -198,34 +234,29 @@ class CacheManager(object):
             locked = True
             _log.debug("Worker started")
             while not self._terminate:
+                self._add_requested_items()
                 item = self._get_item_to_process()
                 if not item:
                     _log.debug("Waiting for work...")
                     self._work_avail.wait(1)
                     _log.debug("... done waiting for work")
                     continue
-                # transition from state REQUESTED to PREPARING
-                item._free = False
-                item._work = True
-                if item._next:
-                    # transition from state PREPARED to ACQUIRED
-                    item._next._wait = True
                 _log.debug("Processing %s..." % str(item._key))
+                item._transition(_CacheItem.REQUESTED, _CacheItem.PREPARING)
+                if item._next:
+                    item._next._transition(_CacheItem.PREPARED, _CacheItem.ACQUIRED)
                 self._lock.release()
                 locked = False
                 item._prepare()
                 self._lock.acquire()
                 locked = True
-                # transition from state PREPARING to PREPARED
-                item._work = False
-                item._wait = False
-                self._work_avail.notify()
+                item._transition(_CacheItem.PREPARING, _CacheItem.PREPARED)
                 if item._next:
-                    # transition from state ACQUIRED to RELEASED
-                    item._next._wait = False
-                    item._next._free = True
+                    item._next._transition(_CacheItem.ACQUIRED, _CacheItem.RELEASED)
+                    item._next = None
+                    self._work_avail.notify()
                 _log.debug("... done processing %s" % item._key)
-                if item._bank == self._l1:
+                if item._bank._id == 1:
                     self._sack_avail.notify()
                 else:
                     self._work_avail.notify()
@@ -235,7 +266,7 @@ class CacheManager(object):
             if locked:
                 self._lock.release()
 
-    def prefetch(self, key, next_value=None):
+    def prefetch(self, key):
         """
         Request item with specified key to be prefetched into L1 cache
         by background thread
@@ -243,24 +274,8 @@ class CacheManager(object):
         try:
             self._lock.acquire()
             _log.debug("prefetch(%s)" % str(key))
-            prev_item = None
-            for bank in self._banks:
-                item = bank._lookup(key)
-                if not item:
-                    item = bank._add(key)
-                    item._next_value = next_value
-                    if prev_item:
-                        prev_item._next = item
-                    bank._access(item)
-                    prev_item = item
-                    self._work_avail.notify()
-                else:
-                    if prev_item:
-                        prev_item._next = item
-                    # transition from state RELEASED to PREPARED
-                    item._free = False
-                    bank._access(item)
-                    break
+            self._prefetch_q.append(key)
+            self._work_avail.notify()
         finally:
             _log.debug("return prefetch(%s)" % str(key))
             self._lock.release()
@@ -274,14 +289,13 @@ class CacheManager(object):
         try:
             self._lock.acquire()
             _log.debug("acquire(%s)" % str(key))
-            item = self._l1._lookup(key)
-            assert item
-            while item._wait:
+            item = self._banks[0]._lookup(key)
+            while not item or item._state != _CacheItem.PREPARED:
                 _log.debug("Waiting on acquire...")
                 self._sack_avail.wait(1)
+                item = self._banks[0]._lookup(key)
             _log.debug("... done waiting on acquire")
-            # transition from state PREPARED to ACQUIRED
-            item._wait = True
+            item._transition(_CacheItem.PREPARED, _CacheItem.ACQUIRED)
             return item._value
         finally:
             _log.debug("return acquire(%s)" % str(key))
@@ -295,10 +309,9 @@ class CacheManager(object):
         try:
             self._lock.acquire()
             _log.debug("release(%s)" % str(key))
-            item = self._l1._lookup(key)
-            # transition from state ACQUIRED to RELEASED
-            item._wait = False
-            item._free = True
+            item = self._banks[0]._lookup(key)
+            item._transition(_CacheItem.ACQUIRED, _CacheItem.RELEASED)
+            item._bank._access(item)
             self._work_avail.notify()
         finally:
             _log.debug("return release(%s)" % str(key))
