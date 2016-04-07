7f1818044b5e5f4c8e5ad0bb647122abe23cff70
Michael Simacek <msimacek@redhat.com>
2016-04-07 16:16:41 +0200

    [aux] Change local-build script

--- aux/local-build.sh
+++ aux/local-build.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
 set -e
-
 cd "$(git rev-parse --show-toplevel)"
-MOCK_CONFIG=epel-7-x86_64
+MOCK_CONFIG="${MOCK_CONFIG:-epel-7-x86_64}"
 VERSION="$(python setup.py -V)"
 RELNO="$(git rev-list "$(git describe --tags --abbrev=0)..HEAD" --count)"
 USERNAME="$(sed 's/@.*//' <<< "$EMAIL")"
-test -n "$USERNAME"
 git archive HEAD --prefix="koschei-$VERSION/"| gzip >koschei-${VERSION}.tar.gz
-mkdir -p build
+rm -r build
+mkdir build
 cd build
 sed "s/^Release:[^%]*/&.$RELNO/" ../koschei.spec > koschei.spec
 rpmbuild -bs -D"_sourcedir $PWD/.." -D"_srcrpmdir $PWD" koschei.spec
-mock --yum -r $MOCK_CONFIG -n --rebuild koschei-${VERSION}-*.$RELNO.*.src.rpm --resultdir .
-createrepo_c .
-RPM=`echo koschei-${VERSION}-*.$RELNO.*.noarch.rpm`
-scp -r repodata "$RPM" "$USERNAME@fedorapeople.org:public_html/koschei/repo/"
-echo "https://$USERNAME.fedorapeople.org/koschei/repo/$RPM"
+mock -r "$MOCK_CONFIG" -n koschei-${VERSION}-*.$RELNO.*.src.rpm --resultdir .
