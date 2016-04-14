a5c587b76839e5f9ed7d488ae5c563b73b87ada2
Michael Simacek <msimacek@redhat.com>
2016-04-14 10:55:17 +0200

    [aux] Update aux scripts

--- aux/debug_frontend.py
+++ aux/debug_frontend.py
@@ -4,6 +4,9 @@ import logging
 import time
 from sqlalchemy import event
 from sqlalchemy.engine import Engine
+from koschei.config import load_config
+
+load_config(['config.cfg.template', 'aux/test-config.cfg'])
 
 from koschei.frontend import app as application
 import koschei.frontend.views
@@ -11,6 +14,7 @@ import koschei.frontend.auth
 
 logger = logging.getLogger("koschei.sql")
 logger.setLevel(logging.DEBUG)
+
 
 @event.listens_for(Engine, "before_cursor_execute")
 def before_cursor_execute(conn, cursor, statement, 
