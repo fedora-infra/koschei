545e17191087cd660dbc56a40159d07f053f19cc
Mikolaj Izdebski <mizdebsk@redhat.com>
2016-05-05 10:07:51 +0200

    Fix aux/local-build.sh: use tito

--- aux/local-build.sh
+++ aux/local-build.sh
@@ -2,13 +2,7 @@
 set -e
 cd "$(git rev-parse --show-toplevel)"
 MOCK_CONFIG="${MOCK_CONFIG:-epel-7-x86_64}"
-VERSION="$(python setup.py -V)"
-RELNO="$(git rev-list "$(git describe --tags --abbrev=0)..HEAD" --count)"
-USERNAME="$(sed 's/@.*//' <<< "$EMAIL")"
-git archive HEAD --prefix="koschei-$VERSION/"| gzip >koschei-${VERSION}.tar.gz
 rm -rf build
-mkdir build
-cd ./build
-sed "s/^Release:[^%]*/&.$RELNO/" ../koschei.spec > koschei.spec
-rpmbuild -bs -D"_sourcedir $PWD/.." -D"_srcrpmdir $PWD" koschei.spec
-mock -r "$MOCK_CONFIG" -n koschei-${VERSION}-*.$RELNO.*.src.rpm --resultdir .
+mkdir -p build/tito/
+tito build --test --srpm -o build/tito/
+mock -r "$MOCK_CONFIG" -n build/tito/koschei-*.src.rpm --resultdir build
