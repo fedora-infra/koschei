1436daa15f8a055f09cbd3721db31578426e47d7
Mikolaj Izdebski <mizdebsk@redhat.com>
2016-06-01 17:20:52 +0200

    [refactor] Simplify repo_cache code

--- koschei/backend/repo_cache.py
+++ koschei/backend/repo_cache.py
@@ -75,133 +75,103 @@ class RepoDescriptor(object):
                           repo_id=self.repo_id, arch=arch)
 
 
-class RepoManager(object):
-    def __init__(self, repo_dir):
-        self._repo_dir = repo_dir
-
-    def _get_repo_dir(self, repo_descriptor):
-        return os.path.join(self._repo_dir, str(repo_descriptor))
-
-    def destroy(self, repo_descriptor, _):
-        repo_dir = self._get_repo_dir(repo_descriptor)
-        if os.path.exists(repo_dir):
-            shutil.rmtree(repo_dir)
-
-    # Download given repo_id from Koji to disk
-    def create(self, repo_descriptor, _):
-        self.destroy(repo_descriptor, None)
-        repo_dir = self._get_repo_dir(repo_descriptor)
-        temp_dir = repo_dir + ".tmp"
-        if os.path.exists(temp_dir):
-            shutil.rmtree(temp_dir)
-        os.mkdir(temp_dir)
-        try:
-            log.debug('Downloading repo {} from Koji to disk'.
-                      format(repo_descriptor))
-            h = librepo.Handle()
-            h.destdir = temp_dir
-            # pylint:disable=no-member
-            h.repotype = librepo.LR_YUMREPO
-            h.urls = [repo_descriptor.make_url()]
-            h.yumdlist = ['primary', 'filelists', 'group', 'group_gz']
-            h.perform(librepo.Result())
-            os.rename(temp_dir, repo_dir)
-            log.debug('Repo {} successfully downloaded to disk'.format(repo_descriptor))
-            return repo_dir
-        except librepo.LibrepoException as e:
-            if e.args[0] == REPO_404:
-                log.debug('Repo {} was not found (url={})'.format(repo_descriptor,
-                                                                  h.urls[0]))
-                return None
-            raise
-
-    def populate_cache(self):
-        repos = []
-        log.debug('Reading cached repos from disk...')
-        for repo_name in os.listdir(self._repo_dir):
-            repo_descriptor = RepoDescriptor.from_string(repo_name)
-            repo_path = os.path.join(self._repo_dir, repo_name)
-            if repo_descriptor:
-                repos.append((repo_descriptor, repo_path))
-            else:
-                # Try to remove files that don't look like valid
-                # repos, such as incomplete downloads.
-                try:
-                    if os.path.isdir(repo_path):
-                        shutil.rmtree(repo_path)
-                    else:
-                        os.remove(repo_path)
-                    log.debug('Removed bogus file from cache: {}'.format(repo_path))
-                except Exception:
-                    log.debug('Unable to remove bogus file from cache: {}'
-                              .format(repo_path))
-        log.debug('Added {} repos from disk'.format(len(repos)))
-        return repos
-
-
-class SackManager(object):
-    def create(self, repo_descriptor, repo_dir):
-        """ Load repo from disk into memory as sack """
-        if not repo_dir:
-            return
-        try:
-            cache_dir = os.path.join(repo_dir, 'cache')
-            build_cache = not os.path.exists(cache_dir)
-            for_arch = get_config('dependency.resolve_for_arch')
-            arch = get_config('dependency.repo_arch')
-            if build_cache:
-                os.mkdir(cache_dir)
-            sack = hawkey.Sack(arch=for_arch, cachedir=cache_dir)
-            log.debug('Loading repo {} from disk into memory'.
-                      format(repo_descriptor))
-            h = librepo.Handle()
-            h.local = True
-            # pylint:disable=no-member
-            h.repotype = librepo.LR_YUMREPO
-            h.urls = [repo_dir]
-            h.yumdlist = ['primary', 'filelists', 'group']
-            repodata = h.perform(librepo.Result()).yum_repo
-            repo = hawkey.Repo('{}-{}'.format(repo_descriptor, arch))
-            repo.repomd_fn = repodata['repomd']
-            repo.primary_fn = repodata['primary']
-            repo.filelists_fn = repodata['filelists']
-            sack.load_yum_repo(repo, load_filelists=True, build_cache=build_cache)
-            log.debug('Repo {} successfully loaded into memory'
-                      .format(repo_descriptor))
-            build_cache = False
-            return sack
-        except (librepo.LibrepoException, IOError):
-            log.debug('Repo {} could not be loaded'.format(repo_descriptor))
-        finally:
-            if build_cache and os.path.exists(cache_dir):
-                shutil.rmtree(cache_dir)
-
-
 class RepoCache(object):
-    def __init__(self, repo_dir=None):
-        if not repo_dir:
-            repo_dir = os.path.join(get_config('directories.cachedir'), 'repodata')
+    def __init__(self):
+        self._repos_dir = os.path.join(get_config('directories.cachedir'), 'repodata')
+        self._capacity = get_config('dependency.cache_l2_capacity')
+        self._repos = OrderedDict()
 
-        dep_config = get_config('dependency')
-        self.cache_l2_capacity = dep_config['cache_l2_capacity']
+        for repo_descriptor, repo_path in sorted(self._read_cache()):
+            self._ensure_capacity()
+            self._repos[repo_descriptor] = repo_path
+        log.debug('Added {} repos from disk'.format(len(self._repos)))
 
-        self.sack_manager = SackManager()
-        self.repo_manager = RepoManager(repo_dir)
+    def _read_cache(self):
+        """ Read cached repos from disk. Remove directories which names
+        are not parseable as repo descriptor strings. """
+        log.debug('Reading cached repos from disk...')
+        for repo_descriptor, repo_path in [(RepoDescriptor.from_string(repo_name),
+                                            os.path.join(self._repos_dir, repo_name))
+                                           for repo_name in os.listdir(self._repos_dir)]:
+            if repo_descriptor:
+                yield repo_descriptor, repo_path
+            elif os.path.exists(repo_path):
+                # Remove directories that don't look like valid repos,
+                # such as incomplete downloads.
+                shutil.rmtree(repo_path)
+                log.debug('Removed bogus file from cache: {}'.format(repo_path))
 
-        self.repos = OrderedDict(sorted(self.repo_manager.populate_cache()))
-        while len(self.repos) > self.cache_l2_capacity:
-            repo_descriptor, repo_path = self.repos.popitem(last=False)
-            self.repo_manager.destroy(repo_descriptor, repo_path)
+    def _ensure_capacity(self):
+        """ Make sure there is enough room for new repo to be added. """
+        if len(self._repos) == self._capacity:
+            # Adding repo would exceed capacity, discard LRU repo to free space.
+            _, repo_path = self._repos.popitem(last=False)
+            if os.path.exists(repo_path):
+                shutil.rmtree(repo_path)
+        assert len(self._repos) < self._capacity
+
+    def _download_repo(self, repo_descriptor, repo_dir):
+        """ Download repo from Koji to disk. """
+        log.debug('Downloading repo {} from Koji to disk'.format(repo_descriptor))
+        h = librepo.Handle()
+        h.destdir = repo_dir
+        # pylint:disable=no-member
+        h.repotype = librepo.LR_YUMREPO
+        h.urls = [repo_descriptor.make_url()]
+        h.yumdlist = ['primary', 'filelists', 'group', 'group_gz']
+        h.perform(librepo.Result())
+        log.debug('Repo {} successfully downloaded to disk'.format(repo_descriptor))
+
+    def _load_sack(self, repo_descriptor, repo_path, build_cache=False):
+        """ Load repo from disk into memory as sack. """
+        cache_dir = os.path.join(repo_path, 'cache')
+        for_arch = get_config('dependency.resolve_for_arch')
+        if build_cache:
+            os.mkdir(cache_dir)
+        sack = hawkey.Sack(arch=for_arch, cachedir=cache_dir)
+        log.debug('Loading repo {} from disk into memory'.format(repo_descriptor))
+        h = librepo.Handle()
+        h.local = True
+        # pylint:disable=no-member
+        h.repotype = librepo.LR_YUMREPO
+        h.urls = [repo_path]
+        h.yumdlist = ['primary', 'filelists', 'group']
+        repodata = h.perform(librepo.Result()).yum_repo
+        repo = hawkey.Repo(repo_descriptor)
+        repo.repomd_fn = repodata['repomd']
+        repo.primary_fn = repodata['primary']
+        repo.filelists_fn = repodata['filelists']
+        sack.load_yum_repo(repo, load_filelists=True, build_cache=build_cache)
+        log.debug('Repo {} successfully loaded into memory'.format(repo_descriptor))
+        return sack
 
     def get_sack(self, repo_descriptor):
-        repo_path = self.repos.get(repo_descriptor)
+        """ Load repo as hawkey sack.  Returns None if repo was not found on Koji.
+        Failures to load repo for any other reason are raised as exceptions.
+
+        Repos are downloaded to temporary directory, which is atomically renamed to
+        final repo directory only after sack is successfully loaded and libsolv
+        cache created. This is to make sure that nonexistent or invalid repos are
+        never present under repo_path, even if process is killed at some point.
+        """
+        repo_path = self._repos.get(repo_descriptor)
         if repo_path:
-            del self.repos[repo_descriptor]
-        else:
-            assert len(self.repos) <= self.cache_l2_capacity
-            while len(self.repos) >= self.cache_l2_capacity:
-                repo_descriptor, repo_path = self.repos.popitem(last=False)
-                self.repo_manager.destroy(repo_descriptor, repo_path)
-            repo_path = self.repo_manager.create(repo_descriptor, None)
-        self.repos[repo_descriptor] = repo_path
-        return self.sack_manager.create(repo_descriptor, repo_path)
+            # Mark repo as most-recently used by re-adding it to OrderedDict.
+            del self._repos[repo_descriptor]
+            self._repos[repo_descriptor] = repo_path
+            return self._load_sack(repo_descriptor, repo_path)
+        try:
+            self._ensure_capacity()
+            repo_path = os.path.join(self._repos_dir, str(repo_descriptor))
+            temp_dir = repo_path + ".tmp"
+            os.mkdir(temp_dir)
+            self._download_repo(repo_descriptor, temp_dir)
+            sack = self._load_sack(repo_descriptor, temp_dir, build_cache=True)
+            os.rename(temp_dir, repo_path)
+            self._repos[repo_descriptor] = repo_path
+            return sack
+        except librepo.LibrepoException as e:
+            if e.args[0] == REPO_404:
+                log.debug('Repo {} was not found'.format(repo_descriptor))
+                return None
+            raise
