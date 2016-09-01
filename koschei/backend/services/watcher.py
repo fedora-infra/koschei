2962f6d13f41f3ead2ed80d2e208d7870160b1f4
Michael Simacek <msimacek@redhat.com>
2016-09-01 14:30:44 +0200

    [watcher] Fix registering real builds

--- koschei/backend/services/watcher.py
+++ koschei/backend/services/watcher.py
@@ -58,7 +58,8 @@ class Watcher(KojiService):
         if pkg:
             newer_build = self.backend.get_newer_build_if_exists(pkg)
             if newer_build:
-                self.backend.register_real_builds([(pkg.id, newer_build)])
+                self.backend.register_real_builds(pkg.collection,
+                                                  [(pkg.id, newer_build)])
 
     def notify_watchdog(self):
         if not get_config('services.watcher.watchdog'):
