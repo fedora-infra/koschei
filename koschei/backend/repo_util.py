461cd0d8e24cfffe2cd44bfc7fe6f053c3994e6f
Michael Simacek <msimacek@redhat.com>
2017-01-23 11:18:42 +0100

    [repo_util] Remove group_gz

--- koschei/backend/repo_util.py
+++ koschei/backend/repo_util.py
@@ -92,7 +92,7 @@ def get_repo(repo_dir, repo_descriptor, download=False):
     h.repotype = librepo.LR_YUMREPO
     h.urls = [repo_descriptor.url if download else repo_path]
     h.local = not download
-    h.yumdlist = ['primary', 'filelists', 'group', 'group_gz']
+    h.yumdlist = ['primary', 'filelists', 'group']
     result = librepo.Result()
     try:
         result = h.perform(result)
