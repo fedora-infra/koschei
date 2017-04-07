b5d608e6662d7b3d76b5e755573ca8d7588edc27
Michael Simacek <msimacek@redhat.com>
2017-04-07 15:45:53 +0200

    [frontend_test] Add tests for group operations

--- test/frontend_test.py
+++ test/frontend_test.py
@@ -157,3 +157,154 @@ class FrontendTest(DBTest):
         self.assertEqual(200, reply.status_code)
         self.assertIn('Packages added: xpp3', reply.data.decode('utf-8'))
         self.assertTrue(pkg.tracked)
+
+    def test_create_group_unauth(self):
+        reply = self.client.post(
+            'add-group',
+            data=dict(
+                name='foo',
+                packages='rnv,eclipse',
+                owners='jdoe,user1',
+            ),
+            follow_redirects=True,
+        )
+        self.assertEqual(501, reply.status_code)
+        self.assertEqual(
+            0,
+            self.db.query(PackageGroup).filter_by(name='foo').count(),
+        )
+
+    @authenticate
+    def test_create_group(self):
+        pkgs = set(self.prepare_packages('rnv', 'eclipse'))
+        self.db.commit()
+        reply = self.client.post(
+            'add-group',
+            data=dict(
+                name='foo',
+                packages='rnv,eclipse',
+                owners='jdoe,user1',
+            ),
+            follow_redirects=True,
+        )
+        self.assertEqual(200, reply.status_code)
+        self.assertIn('foo', reply.data.decode('utf-8'))
+        self.assertIn('eclipse', reply.data.decode('utf-8'))
+        group = self.db.query(PackageGroup).filter_by(name='foo').one()
+        self.assertEquals('jdoe', group.namespace)
+        self.assertEquals({p.base for p in pkgs}, set(group.packages))
+        self.assertEquals(
+            {self.prepare_user(name='jdoe'), self.prepare_user(name='user1')},
+            set(group.owners),
+        )
+
+    @authenticate
+    def test_create_group_missing_pkg(self):
+        self.prepare_packages('rnv')
+        self.db.commit()
+        reply = self.client.post(
+            'add-group',
+            data=dict(
+                name='foo',
+                packages='rnv,eclipse',
+                owners='jdoe,user1',
+            ),
+            follow_redirects=True,
+        )
+        self.assertEqual(200, reply.status_code)
+        self.assertIn("Packages don&#39;t exist: eclipse", reply.data.decode('utf-8'))
+        self.assertIn('Create new group', reply.data.decode('utf-8'))
+
+    @authenticate
+    def test_edit_group(self):
+        pkgs = self.prepare_packages('rnv', 'eclipse', 'maven')
+        group = self.prepare_group(
+            namespace='jdoe',
+            name='foo',
+            owners=['jdoe', 'user1'],
+            content=['rnv', 'eclipse'],
+        )
+        reply = self.client.post(
+            'groups/jdoe/foo/edit',
+            data=dict(
+                name='bar',
+                packages='eclipse,maven',
+                owners='jdoe,user2',
+            ),
+            follow_redirects=True,
+        )
+        self.assertEqual(200, reply.status_code)
+        self.assertEqual(
+            0,
+            self.db.query(PackageGroup).filter_by(name='foo').count(),
+        )
+        group = self.db.query(PackageGroup).filter_by(name='bar').one()
+        self.assertEquals('jdoe', group.namespace)
+        self.assertEquals({p.base for p in pkgs[1:]}, set(group.packages))
+        self.assertEquals(
+            {self.prepare_user(name='jdoe'), self.prepare_user(name='user2')},
+            set(group.owners),
+        )
+
+    @authenticate
+    def test_edit_group_unpermitted(self):
+        pkgs = self.prepare_packages('rnv', 'eclipse', 'maven')
+        group = self.prepare_group(
+            namespace='user1',
+            name='foo',
+            owners=['user1'],
+            content=['rnv', 'eclipse'],
+        )
+        reply = self.client.post(
+            'groups/user1/foo/edit',
+            data=dict(
+                name='foo',
+                packages='eclipse,maven',
+                owners='jdoe,user2',
+            ),
+            follow_redirects=True,
+        )
+        self.assertEqual(200, reply.status_code)
+        self.assertIn("You don&#39;t have permission", reply.data.decode('utf-8'))
+        self.assertEquals('user1', group.namespace)
+        self.assertEquals({p.base for p in pkgs[:2]}, set(group.packages))
+        self.assertEquals(
+            {self.prepare_user(name='user1')},
+            set(group.owners),
+        )
+
+    @authenticate
+    def test_delete_group(self):
+        self.prepare_packages('rnv', 'eclipse')
+        self.prepare_group(
+            namespace='jdoe',
+            name='foo',
+            owners=['jdoe'],
+            content=['rnv', 'eclipse'],
+        )
+        self.client.post(
+            'groups/jdoe/foo/delete',
+            follow_redirects=True,
+        )
+        self.assertEqual(
+            0,
+            self.db.query(PackageGroup).filter_by(name='foo').count(),
+        )
+
+    @authenticate
+    def test_delete_group_unpermitted(self):
+        self.prepare_packages('rnv', 'eclipse')
+        self.prepare_group(
+            namespace='user1',
+            name='foo',
+            owners=['user1'],
+            content=['rnv', 'eclipse'],
+        )
+        self.client.post(
+            'groups/user1/foo/delete',
+            follow_redirects=True,
+        )
+        self.assertEqual(
+            1,
+            self.db.query(PackageGroup).filter_by(name='foo').count(),
+        )
