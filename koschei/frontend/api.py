291c5a52e3edd5093a29ff7abbfd4b43abb5f9ed
Michael Simacek <msimacek@redhat.com>
2017-04-21 13:26:48 +0200

    [api] Try to improve list_packages performance

--- koschei/frontend/api.py
+++ koschei/frontend/api.py
@@ -16,48 +16,50 @@
 #
 # Author: Mikolaj Izdebski <mizdebsk@redhat.com>
 
-from flask import jsonify, request
-from sqlalchemy.orm import contains_eager
-from koschei.util import merge_sorted
+import json
+
+from flask import request, Response
 from koschei.frontend import app, db
-from koschei.models import Package, Collection, Build
-
-
-def build_to_json(build):
-    if build:
-        return {'task_id': build.task_id}
-
-
-def package_to_json(package):
-    if package:
-        return {'name': package.name,
-                'collection': package.collection.name,
-                'state': package.state_string,
-                'last_complete_build': build_to_json(package.last_complete_build)}
-
-
-def packages_to_json(packages):
-    return [package_to_json(p) for p in packages]
+from koschei.models import Package, Collection, Build, get_package_state
 
 
 @app.route('/api/v1/packages')
 def list_packages():
-    def run_query(query):
-        query = query.join(Collection, Collection.id == Package.collection_id)\
-            .options(contains_eager(Package.collection))
-        if 'name' in request.args:
-            query = query.filter(Package.name.in_(request.args.getlist('name')))
-        if 'collection' in request.args:
-            query = query.filter(Collection.name.in_(request.args.getlist('collection')))
-        return query.order_by(Package.name).all()
+    def prepare_package_entry(entry):
+        return {
+            'name': entry.name,
+            'collection': entry.collection_name,
+            'state': get_package_state(
+                tracked=entry.tracked,
+                blocked=entry.blocked,
+                resolved=entry.resolved,
+                last_complete_build_state=entry.last_complete_build_state,
+            ),
+            'last_complete_build': {
+                'task_id': entry.build_task_id,
+            } if entry.build_task_id is not None else None,
+        }
 
-    pkgs_with_complete_build = db.query(Package)\
-        .join(Build, Build.package_id == Package.id)\
-        .filter(Build.last_complete)\
-        .options(contains_eager(Package.last_complete_build))
-    pkgs_without_complete_build = db.query(Package)\
-        .filter(Package.last_complete_build_id == None)
-    packages = merge_sorted(*[run_query(query) for query in pkgs_with_complete_build,
-                            pkgs_without_complete_build],
-                            key=lambda p: p.name)
-    return jsonify(packages_to_json(packages))
+    query = (
+        db.query(
+            Package.name, Package.collection_id, Package.resolved,
+            Package.tracked, Package.blocked, Package.last_complete_build_state,
+            Build.task_id.label('build_task_id'),
+            Collection.name.label('collection_name'),
+        )
+        .outerjoin(
+            Build,
+            (Package.last_complete_build_id == Build.id) & Build.last_complete
+        )
+        .join(Collection)
+        .order_by(Package.name)
+    )
+    if 'name' in request.args:
+        query = query.filter(Package.name.in_(request.args.getlist('name')))
+    if 'collection' in request.args:
+        query = query.filter(Collection.name.in_(request.args.getlist('collection')))
+
+    return Response(
+        json.dumps([prepare_package_entry(p) for p in query]),
+        mimetype='application/json',
+    )
