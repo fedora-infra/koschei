9d8be6557aaaefe649de846b6562533a57e6a986
Mikolaj Izdebski <mizdebsk@redhat.com>
2017-11-03 18:10:00 +0100

    Follow builddep logic for installing BuildRequires
    For installing BuildRequires Koji uses DNF builddep plugin, which
    doesn't use get_best_selector().  It first tries to install
    BuildRequires as reldep and falls back to file if reldep doesn't match
    anything and dependency starts with a slash.
    
    See: https://github.com/rpm-software-management/dnf-plugins-core/blob/2.1.4/plugins/builddep.py#L138-L153
    
    Closes #200

--- koschei/backend/depsolve.py
+++ koschei/backend/depsolve.py
@@ -19,66 +19,20 @@
 
 from __future__ import print_function, absolute_import
 
-import re
 import hawkey
 
 from koschei.config import get_config
 
 
-def _get_best_selector(sack, dep):
-    # Based on get_best_selector in dnf's subject.py
-    # pylint: disable=len-as-condition
-
-    def is_glob_pattern(pattern):
-        return set(pattern) & set("*[?")
-
-    def first(iterable):
-        it = iter(iterable)
-        try:
-            return next(it)
-        except StopIteration:
-            return None
-
-    def _nevra_to_selector(sltr, nevra):
-        if nevra.name is not None:
-            if is_glob_pattern(nevra.name):
-                sltr.set(name__glob=nevra.name)
-            else:
-                sltr.set(name=nevra.name)
-        if nevra.version is not None:
-            evr = nevra.version
-            if nevra.epoch is not None and nevra.epoch > 0:
-                evr = "%d:%s" % (nevra.epoch, evr)
-            if nevra.release is None:
-                sltr.set(version=evr)
-            else:
-                evr = "%s-%s" % (evr, nevra.release)
-                sltr.set(evr=evr)
-        if nevra.arch is not None:
-            sltr.set(arch=nevra.arch)
-        return sltr
-
-    subj = hawkey.Subject(dep)
+def _get_builddep_selector(sack, dep):
+    # Try to find something by provides
     sltr = hawkey.Selector(sack)
-
-    nevra = first(subj.nevra_possibilities_real(sack, allow_globs=True))
-    if nevra:
-        s = _nevra_to_selector(sltr, nevra)
-        if len(s.matches()) > 0:
-            return s
-
-    # pylint: disable=E1101
-    reldep = first(subj.reldep_possibilities_real(sack))
-    if reldep:
-        dep = str(reldep)
-        s = sltr.set(provides=dep)
-        if len(s.matches()) > 0:
-            return s
-
-    if re.search(r'^\*?/', dep):
-        key = "file__glob" if is_glob_pattern(dep) else "file"
-        return sltr.set(**{key: dep})
-
+    sltr.set(provides=dep)
+    found = sltr.matches()
+    if not found and dep.startswith("/"):
+        # Nothing matches by provides and since it's file, try by files
+        sltr = hawkey.Selector(sack)
+        sltr.set(file=dep)
     return sltr
 
 
@@ -87,11 +41,11 @@ def run_goal(sack, br, group):
     goal = hawkey.Goal(sack)
     problems = []
     for name in group:
-        sltr = _get_best_selector(sack, name)
+        sltr = _get_builddep_selector(sack, name)
         # missing packages are silently skipped as in dnf
         goal.install(select=sltr)
     for r in br:
-        sltr = _get_best_selector(sack, r)
+        sltr = _get_builddep_selector(sack, r)
         # pylint: disable=E1103
         if not sltr.matches():
             problems.append("No package found for: {}".format(r))
@@ -122,7 +76,7 @@ def compute_dependency_distances(sack, br, deps):
     level = 1
     # pylint:disable=E1103
     pkgs_on_level = {x for r in br for x in
-                     _get_best_selector(sack, r).matches()}
+                     _get_builddep_selector(sack, r).matches()}
     while pkgs_on_level:
         for pkg in pkgs_on_level:
             dep = dep_map.get(pkg.name)
